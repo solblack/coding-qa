6329d75203723a9275e3d78809fd7aa1228fb97d
Fabricio Di Gennaro <fabriciodigennaro9@gmail.com>
2021-03-10 22:21:04 -0300

    new questions

--- src/app/components/questions/questions.component.ts
+++ src/app/components/questions/questions.component.ts
@@ -14,7 +14,15 @@ interface Question{
 export class QuestionsComponent implements OnInit {
   questions: Question[] = [
     { id: 1, question: 'Cómo se declara una variable en JS?', answer: 'Answer'},
-    { id: 2, question: 'Qué es la asincronía?', answer: 'Answer'}
+    { id: 2, question: 'Qué es la asincronía?', answer: 'Answer'},
+    { id: 3, question: 'Cómo manejar la asincronía en JS?', answer: 'Answer'},
+    { id: 4, question: 'Qué es el package.json?', answer: 'Answer'},
+    { id: 5, question: 'Qué es NPM?', answer: 'Answer'},
+    { id: 6, question: 'Qué es Ecmascript?', answer: 'Answer'},
+    { id: 7, question: 'Qué es un middleware en express?', answer: 'Answer'},
+    { id: 8, question: 'Qué librerias han usado para hacer el flow de autenticación?', answer: 'Answer'},
+    { id: 9, question: 'Qué es una clase?', answer: 'Answer'},
+    { id: 10, question: 'Qué como crear una instancia de una clase?', answer: 'Answer'},
   ]
   constructor() { }
 
